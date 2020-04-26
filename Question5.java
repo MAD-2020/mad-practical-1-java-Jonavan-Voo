d8483a69a25e577c1e9e5a90dc27ebc151fb469e
Jonavan-Voo <64002875+Jonavan-Voo@users.noreply.github.com>
2020-04-26 14:43:19 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,39 @@
-import java.util.Scanner;
-
-public class Question5
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
+import java.util.Scanner; 
+import java.util.ArrayList;
+public class Question5 {
+  public static void main(String[] args) {
+    //create list
+    ArrayList<Integer> lst = new ArrayList<>(); 
+    //scanner to get user input
+    Scanner myObj = new Scanner(System.in);
+    //prompt user for number of integers
+    System.out.print("Enter number of integers to input: ");
+    int numbers = myObj.nextInt(); //store input
+    //get inputs from user and add to list
+    for (int i = 0; i < numbers; i++) {
+      System.out.print("Enter number: ");
+      lst.add(myObj.nextInt());
+    } 
+    myObj.close(); //close scanner
+    //initialise mode number and number of occurances
+    int modenumber = 0;
+    int modecount = 0;
+    //check occurances for each number in list
+    for (int i = 0; i < lst.size(); i++) {
+      int count = 0;
+      for (int x = 0; x < lst.size(); x++) {
+        if (lst.get(x) == lst.get(i)) {
+          count++;
+        }
+      }
+      //replace mode number and number of occurances 
+      //if count more than modecount
+      if (count > modecount) {
+        modecount = count;
+        modenumber = lst.get(i);
+      }
+    }
+    //print mode
+    System.out.println("Mode: " + modenumber);
   }
 }
