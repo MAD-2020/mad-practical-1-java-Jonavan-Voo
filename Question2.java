a35517ddd61efe666d6eb0f65f345106c9d3f325
Jonavan-Voo <64002875+Jonavan-Voo@users.noreply.github.com>
2020-04-26 14:36:58 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;
-
-public class Question2
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompts user for height and weight, calculate and print out the height and weight of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+import java.util.Scanner; 
+class Main {
+  public static void main(String[] args) {
+    //scanner to get user input
+    Scanner myObj = new Scanner(System.in);
+    //prompt user for height and weight
+    System.out.print("Enter height (in m): ");
+    float height = myObj.nextFloat(); //store user height
+    System.out.print("Enter weight (in kg): "); //store user weight
+    float weight = myObj.nextFloat();
+    myObj.close(); //close scanner
+    //print user's height and weight
+    System.out.println("Height: " + height + "m"); 
+    System.out.println("Weight: " + weight + "kg"); 
   }
 }
+
